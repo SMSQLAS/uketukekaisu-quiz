0738c6c9fe29b7b08590ba159da7582865efdb7b
SMSQLAS <hikarichozai@shikoku-ms.com>
2026-08-26 15:16:54 +0900

    Update GAS_WEB_APP_URL to new web app URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Apps Script のウェブアプリURLをここに貼り付けてください。
 // 例: const GAS_WEB_APP_URL = "https://script.google.com/macros/s/xxxx/exec";
-const GAS_WEB_APP_URL = "";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxCGQdVIRdGYjocIAdfrMA_gUYtxovXCQBj4IDBKXuUZTP7BKx5z0YVRRHIheUsRTI/exec";
